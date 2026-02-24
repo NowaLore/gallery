9a87a19cda866a07e1528f862ffc572000d36c0c
NowaLore <denisskysmart@gmail.com>
2026-02-24 19:45:34 +0300

    feat: added elements in preview header

--- src/app/componets/preview/preview-params.ts
+++ src/app/componets/preview/preview-params.ts
@@ -5,3 +5,94 @@ export const previewParams = {
     "data-preview": "",
   },
 };
+
+export const headerParams = {
+  tagName: "div",
+  classList: [],
+  attr: {},
+};
+
+export const posterWrapperParams = {
+  tagName: "div",
+  classList: [],
+  attr: {},
+};
+
+export const posterImgParams = {
+  tagName: "img",
+  classList: [],
+  attr: {},
+};
+
+export const heroContainerParams = {
+  tagName: "div",
+  classList: [],
+  attr: {},
+};
+
+export const nameWrapperParams = {
+  tagName: "div",
+  classList: [],
+  attr: {},
+};
+
+export const filmNameParams = {
+  tagName: "span",
+  classList: [],
+  attr: {},
+  text: "",
+};
+
+export const infoContainerParams = {
+  tagName: "div",
+  classList: [],
+  attr: {},
+};
+
+export const typeParams = {
+  tagName: "span",
+  classList: [],
+  attr: {},
+};
+
+export const yearParams = {
+  tagName: "span",
+  classList: [],
+  attr: {},
+};
+
+export const timeParams = {
+  tagName: "span",
+  classList: [],
+  attr: {},
+};
+
+export const previewTagsParams = {
+  tagName: "div",
+  classList: [],
+  attr: {},
+};
+
+export const tagsItemParams = {
+  tagName: "div",
+  classList: [],
+  attr: {},
+};
+
+export const rateConteinerParams = {
+  tagName: "div",
+  classList: [],
+  attr: {},
+};
+
+export const previewImdbParams = {
+  tagName: "div",
+  classList: [],
+  attr: {},
+};
+
+export const previewAgeParams = {
+  tagName: "div",
+  classList: [],
+  attr: {},
+};
--- src/app/componets/preview/preview.ts
+++ src/app/componets/preview/preview.ts
@@ -1,5 +1,22 @@
 import { Creator } from "../../../utilities/creator";
-import { previewParams } from "./preview-params";
+import {
+  filmNameParams,
+  headerParams,
+  heroContainerParams,
+  infoContainerParams,
+  nameWrapperParams,
+  posterImgParams,
+  posterWrapperParams,
+  previewParams,
+  typeParams,
+  yearParams,
+  timeParams,
+  previewTagsParams,
+  tagsItemParams,
+  rateConteinerParams,
+  previewImdbParams,
+  previewAgeParams,
+} from "./preview-params";
 
 export class Preview {
   preview;
@@ -13,7 +30,58 @@ export class Preview {
     return this.preview;
   }
 
-  headerCreator() {}
+  builder() {
+    const header = this.headerCreator();
+    this.preview?.append(header);
+  }
+
+  headerCreator() {
+    //Создать на основе this.data хедер
+    const headerElement = new Creator(headerParams).getElement() as HTMLElement;
+    const posterWrapper = new Creator(
+      posterWrapperParams,
+    ).getElement() as HTMLElement;
+    const posterImg = new Creator(posterImgParams).getElement() as HTMLElement;
+    const heroContainer = new Creator(
+      heroContainerParams,
+    ).getElement() as HTMLElement;
+    const nameWrapper = new Creator(
+      nameWrapperParams,
+    ).getElement() as HTMLElement;
+    const filmName = new Creator(filmNameParams).getElement() as HTMLElement;
+    const infoContainer = new Creator(
+      infoContainerParams,
+    ).getElement() as HTMLElement;
+    const type = new Creator(typeParams).getElement() as HTMLElement;
+    const year = new Creator(yearParams).getElement() as HTMLElement;
+    const time = new Creator(timeParams).getElement() as HTMLElement;
+    const previewTags = new Creator(
+      previewTagsParams,
+    ).getElement() as HTMLElement;
+    const tagsItem = new Creator(tagsItemParams).getElement() as HTMLElement;
+    const rateContainer = new Creator(
+      rateConteinerParams,
+    ).getElement() as HTMLElement;
+    const previewImdb = new Creator(
+      previewImdbParams,
+    ).getElement() as HTMLElement;
+    const previewAge = new Creator(
+      previewAgeParams,
+    ).getElement() as HTMLElement;
+    headerElement.append(posterWrapper, heroContainer);
+    posterWrapper.append(posterImg);
+    heroContainer.append(
+      nameWrapper,
+      infoContainer,
+      previewTags,
+      rateContainer,
+    );
+    nameWrapper.append(filmName);
+    infoContainer.append(type, year, time);
+    previewTags.append(tagsItem);
+    rateContainer.append(previewImdb, previewAge);
+    return headerElement;
+  }
 
   setData(data) {
     this.data = data;
